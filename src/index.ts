150884b589d3da04fb69100dcfe8914f913d7de4
Igor.tdf <104040266+IgorTDF@users.noreply.github.com>
2022-05-09 16:30:47 -0300

    parimpar

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,19 @@ debe volver a pedir el número por
 teclado (hasta que se ingrese un
 número mayor que cero)*/
 
-let numeroUsuario: number = Number(prompt("Ingrese un numero entero"));
+let numeroUsuario: number | null = Number(prompt("Ingrese un numero entero"));
 
-if (numeroUsuario % 2 === 0) {
+if (numeroUsuario === 0) {
+  console.log("El número es 0 - Ingrese otro numero-");
+} else if (numeroUsuario % 2 === 0) {
+  console.log("El número ingresado es par.");
+} else {
+  console.log("El número ingresado es impar.");
+}
+
+/*if (numeroUsuario % 2 === 0) {
   console.log("El numero " + numeroUsuario + " es par");
 } else {
   console.log("El numero " + numeroUsuario + " es impar");
 }
-if (numeroUsuario <= 0) console.log(" Ingrese un numero mayor a cero");
+if (numeroUsuario <= 0) console.log(" Ingrese un numero mayor a cero");*/
